d500d10404dcc323cbe2a6c10f4b867e52a2a69c
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-08 11:58:58 +0200

    Maximum de deux nombres

---
+++ Exercice 3.py
@@ -0,0 +1,8 @@
+def main():
+    a = int(input("nombre: "))
+    b = int(input("nombre: "))
+
+    print(f'maximum de a et b = {a}' if a > b else f'maximum de a et b = {b}')
+
+if __name__ == '__main__':
+    main()
