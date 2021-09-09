3ef3c602717ef81a41afc28bd0d803a7610adec2
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-09 02:38:47 +0200

    Reste et quotient

---
+++ Exercice 13.py
@@ -0,0 +1,9 @@
+def main():
+    a = int(input('a: '))
+    b = int(input('b: '))
+
+    print(f"Reste de la division de {a} / {b} = {a % b}")
+    print(f"Quotient de la division de {a} / {b} = {a // b}")
+
+if __name__ == '__main__':
+    main()
