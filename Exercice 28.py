53e284d4b31370a66316185d4637988183caaa1e
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-09 03:31:37 +0200

    Tester si un tableau ou une chaine est vide

---
+++ Exercice 28.py
@@ -0,0 +1,17 @@
+def main():
+    t = []
+    if not t:
+        print('vide')
+    else:
+        print('pas vide')
+
+    chaine = ''
+
+    if not chaine:
+        print('vide')
+    else:
+        print('pas vide')
+
+
+if __name__ == '__main__':
+    main()
