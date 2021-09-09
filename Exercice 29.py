48bf10c8141befa2cc8e91c81f042a95454891a3
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-09 03:32:38 +0200

    Supprimer les doublons d'une liste

---
+++ Exercice 29.py
@@ -0,0 +1,14 @@
+def removeDuplicate(l):
+    unique = []
+    for x in l:
+        if x not in unique:
+            unique.append(x)
+    return unique
+
+def main():
+    l = [2, 7, 13, 2, 17, 13, 2, 7, 13]
+    print(removeDuplicate(l))
+
+
+if __name__ == '__main__':
+    main()
